33e82c056acb6b8527d215f36f33cba436ccdb15
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-09 21:27:27 +0100

    Add result calculator two

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -1,18 +1,27 @@
-/* becode/javascript
- *
- * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (() => {
-    // to get the value of an input: document.getElementById("element-id").value
+    // to get the value of an input: document.getElementById("element-id").valu
 
     const performOperation = operation => {
+
+    let x = Number(document.getElementById("op-one").value);
+    let y = Number(document.getElementById("op-two").value);
+    let z = 1;
+
+        switch (operation) {
+            case "addition":
+                z = x + y;
+                break;
+            case "substraction":
+                z = x - y;
+                break;
+            case "multiplication":
+                z = x * y;
+                break;
+            case "division":
+                z = x / y;
+                break;
+        }
+        alert(z);
         // perform the operation
     };
 
