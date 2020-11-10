005b85ae8fe1c4bf7a3a2d9e1eec345651704cdd
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-10 14:21:11 +0100

    Change var names

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -3,33 +3,33 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
-        const x = Number(document.getElementById("op-one").value);
-        const y = Number(document.getElementById("op-two").value);
-        const answer = x + y;
+        const numberOne = Number(document.getElementById("op-one").value);
+        const numberTwo = Number(document.getElementById("op-two").value);
+        const answer = numberOne + numberTwo;
         alert(answer);
        // perform an addition
     }); 
 
     document.getElementById("substraction").addEventListener("click", function() {
-        const x = Number(document.getElementById("op-one").value);
-        const y = Number(document.getElementById("op-two").value);
-        const answer = x - y;
+        const numberOne = Number(document.getElementById("op-one").value);
+        const numberTwo = Number(document.getElementById("op-two").value);
+        const answer = numberOne - numberTwo;
         alert(answer);
       // perform an substraction
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        const x = Number(document.getElementById("op-one").value);
-        const y = Number(document.getElementById("op-two").value);
-        const answer = x * y;
+        const numberOne = Number(document.getElementById("op-one").value);
+        const numberTwo = Number(document.getElementById("op-two").value);
+        const answer = numberOne * numberTwo;
         alert(answer);
     // perform an multiplication
     });
 
     document.getElementById("division").addEventListener("click", function() {
-        const x = Number(document.getElementById("op-one").value);
-        const y = Number(document.getElementById("op-two").value);
-        const answer = x / y;
+        const numberOne = Number(document.getElementById("op-one").value);
+        const numberTwo = Number(document.getElementById("op-two").value);
+        const answer = numberOne / numberTwo;
         alert(answer);
       // perform an division
     });
