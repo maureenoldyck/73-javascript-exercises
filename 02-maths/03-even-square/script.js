b5ce4335d9f98f31ec9c5d24424881f5503c4237
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-10 10:16:45 +0100

    Answer even square

--- 02-maths/03-even-square/script.js
+++ 02-maths/03-even-square/script.js
@@ -1,20 +1,10 @@
-/* becode/javascript
- *
- * /02-maths/03-even-square/script.js - 2.3: carrés des pairs
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+    document.getElementById("run").addEventListener("click", function () {
 
-(function() {
-
-    document.getElementById("run").addEventListener("click", function() {
-
-        // your code here
-
+        for (let i = 1; i <= 21; i++) {
+            let numbers = i * i;
+            alert(numbers);
+        } // your code here
     });
-
 })();
