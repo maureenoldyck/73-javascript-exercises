1d51e5bd29f51229a6791b0806bd0e71581f7799
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-10 11:42:20 +0100

    Answer sort numbers

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -1,21 +1,22 @@
-/* becode/javascript
- *
- * /02-maths/04-sort-numbers/script.js - 2.4: classer des nombres
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
-(function() {
+(function () {
 
     // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", function() {
+
+
+    document.getElementById("run").addEventListener("click", function () {
+
+        const numbers = document.getElementById("numbers").value;
+        const array = numbers.split(",");
+
+        array.sort(function (a, b) {
+            return a - b
+        });
 
         // your code here
+
+        document.getElementById("numbers").value = array;
+
 
     });
 
