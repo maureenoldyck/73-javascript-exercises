e9c0d3887bd7d7164f22095c715621471f179463
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-10 13:19:43 +0100

    Answer factorial exercise

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -1,24 +1,26 @@
-/* becode/javascript
- *
- * /02-maths/05-factorial/script.js - 2.5: Factorielle
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
-(function() {
-    
     // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", function() {
+    document.getElementById("run").addEventListener("click", function () {
 
-        
+        function factorialize(num) {
 
-        // your code here
+            if (num === 0 || num === 1) 
+              return 1; 
+
+            for (let i = num - 1; i >= 1; i--)  { 
+                num = num * i;
+            }
+            return num;
+          }
+         
+        let number = parseInt(document.getElementById("number").value);
+        let answer = factorialize(number);
+        alert(answer);
+
 
     });
+
 
 })();
