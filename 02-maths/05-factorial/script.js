1e8ca8ceb08294dfdc5e440d512cbfedad027b69
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-11 16:45:50 +0100

    Add more simple answer

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -1,26 +1,27 @@
 (function () {
 
-    // to get the value of an input: document.getElementById("element-id").value
+  // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("run").addEventListener("click", function () {
-
-        function factorialize(num) {
-
-            if (num === 0 || num === 1) 
-              return 1; 
-
-            for (let i = num - 1; i >= 1; i--)  { 
-                num = num * i;
-            }
-            return num;
-          }
-         
-        let number = document.getElementById("number").value;
-        let answer = factorialize(number);
-        alert(answer);
+  document.getElementById("run").addEventListener("click", function () {
 
 
-    });
+
+
+    let number = document.getElementById("number").value;
+
+    if (number == 0) {
+      number = 1;
+    }
+
+    for (let i = number; i >= 2; i--) {
+      number = number * (i - 1);
+
+
+    }
+    alert(number);
+
+  });
+
 
 
 })();
