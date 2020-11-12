f2c0bed9e4657158e829dca2af5f10e570ad4d0e
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-12 16:47:12 +0100

    Answer walk two

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -1,13 +1,4 @@
-/* becode/javascript
- *
- * /05-arrays/04-walk-two/script.js - 5.4: array traversal (2)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
 
@@ -26,6 +17,12 @@
         "cherry",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function(){
+fruits.forEach((fruit) => {
+    console.log(fruit);
+});
+    
+    });
+
 
 })();
