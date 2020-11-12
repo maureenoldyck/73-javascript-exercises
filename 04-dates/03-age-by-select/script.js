9bdc12cf4d03c621db8b4bf203320ffac6b0de8e
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-12 15:44:00 +0100

    Answer age by select

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
 
+
+
+        let day = Number(document.getElementById("dob-day").value);
+        let month = Number(document.getElementById("dob-month").value) - 1;
+        let year = Number(document.getElementById("dob-year").value);
+        let today = new Date();
+        let yearToday = today.getFullYear();
+        let monthToday = new Date().getMonth();
+        let dayToday = new Date().getDate();
+
+        let age = yearToday - year;
+    
+
+        if (month >= monthToday && day > dayToday) {
+            age = age - 1 ;
+        }
+
+        else if (month > monthToday) {
+            age = age - 1 ;
+        }
+
+        alert("You are " + age + " years old.");
+        // your code here
+    });
 })();
