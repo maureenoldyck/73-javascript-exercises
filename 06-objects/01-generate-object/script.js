a93fdf2578bac97ca9d34fa6fb016e30f7c28308
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-12 16:01:32 +0100

    Answer generate object

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -1,14 +1,20 @@
-/* becode/javascript
- *
- * /06-objects/01-generate-object/script.js - 6.1: générer un object
- *
- * coded by leny@BeCode
- * started at 08/05/2019
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (() => {
+
+    document.getElementById("run").addEventListener("click", function () {
+
+
+        let maureen = {
+            lastname: "oldyck",
+            firstname: "maureen",
+            age: "23",
+            city: "aspelare",
+            country: "belgium"
+
+        };
+
+        console.log(maureen);
+
+    });
+
     // your code here
 })();
