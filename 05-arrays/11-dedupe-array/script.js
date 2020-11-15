761b2d812708220d02b11f280f950081f0764af9
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-15 16:43:12 +0100

    Add second answer dedupe array

--- 05-arrays/11-dedupe-array/script.js
+++ 05-arrays/11-dedupe-array/script.js
@@ -24,7 +24,9 @@
         //Use Array.from to create new array & use new Set to store the unique values from the given array in the new created array
         const uniqueFruits = Array.from(new Set(fruits));
         console.log(uniqueFruits);
-
+/* or 
+console.log([...new Set(fruits)]) ;
+*/
        
     });
 
