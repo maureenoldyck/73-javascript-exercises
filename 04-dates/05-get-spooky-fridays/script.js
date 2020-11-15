c13eac8cf077d02057685efe504961cad98272a0
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-15 15:05:53 +0100

    Answer spooky fridays

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -1,16 +1,24 @@
-/* becode/javascript
- *
- * /04-dates/05-get-spooky-fridays/script.js - 4.5: calcul des vendredi 13
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+    document.getElementById("run").addEventListener("click", function () {
 
-(function() {
 
-    // your code here
+        const givenYear = document.getElementById("year").value;
+        let months = [];
+        let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+        // We create a for loop to loop through all the months of the year
+        for (let i = 0; i < 12; i++) {
+
+            // We create the variable date in order to have all the date information to get the weekday because this is the information we would like to test
+            let date = new Date(givenYear, i, 13);
+            if (date.getDay() == 5) {
+                months.push(month[date.getMonth()]);
+            }
+        };
+
+        alert("There is a Friday the 13th in " + months);
+
+        // your code here
+    });
 })();
