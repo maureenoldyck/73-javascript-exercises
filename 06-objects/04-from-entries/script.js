ae472ab3672a80366e103515439c77a9ec5fd786
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-15 19:18:51 +0100

    Answer from entries

--- 06-objects/04-from-entries/script.js
+++ 06-objects/04-from-entries/script.js
@@ -1,16 +1,22 @@
-/* becode/javascript
- *
- * /06-objects/04-from-entries/script.js - 6.4: fromEntries
- *
- * coded by leny@BeCode
- * started at 08/05/2019
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (() => {
     const keys = ["name", "species", "age", "gender", "color"];
     const values = ["Skitty", "cat", 9, "female", "tabby"];
     // your code here
+
+    document.getElementById("run").addEventListener("click", function () {
+
+        let objectsArray = [];
+
+// use the forEach method to combine the two arrays
+
+        keys.forEach((item, index) =>
+            objectsArray.push([item, values[index]]));
+
+// with the fromEntries we create an object from the new created array
+
+        const object = Object.fromEntries(objectsArray);
+
+        console.log(object);
+    });
+
 })();
