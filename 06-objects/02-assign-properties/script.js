3c58d32861127541273d085d80336c072a02c5a2
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-15 21:05:51 +0100

    Answer assign properties

--- 06-objects/02-assign-properties/script.js
+++ 06-objects/02-assign-properties/script.js
@@ -1,30 +1,65 @@
-/* becode/javascript
- *
- * /06-objects/02-assign-properties/script.js - 6.2: assigner des propriétés
- *
- * coded by leny@BeCode
- * started at 08/05/2019
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (() => {
-    const computers = [
-        {id: "0001", available: false, user: "leny", os: "macOS"},
-        {id: "0002", available: false, user: "Nicolas"},
-        {id: "0003"},
-        {id: "0004", os: "Windows"},
-        {id: "0005"},
-        {id: "0006", os: "macOS"},
-        {id: "0007"},
-        {id: "0008"},
-        {id: "0009", available: false, user: "Anthony"},
+    const computers = [{
+            id: "0001",
+            available: false,
+            user: "leny",
+            os: "macOS"
+        },
+        {
+            id: "0002",
+            available: false,
+            user: "Nicolas"
+        },
+        {
+            id: "0003"
+        },
+        {
+            id: "0004",
+            os: "Windows"
+        },
+        {
+            id: "0005"
+        },
+        {
+            id: "0006",
+            os: "macOS"
+        },
+        {
+            id: "0007"
+        },
+        {
+            id: "0008"
+        },
+        {
+            id: "0009",
+            available: false,
+            user: "Anthony"
+        },
     ];
     const defaultProps = {
         available: true,
         os: "linux",
         user: null,
     };
+
+
+    document.getElementById("run").addEventListener("click", function () {
+
+
+        // Use the for loop to merge the two objects together   
+        for (let i = 0; i < computers.length; i++) {
+
+
+            // When merging use the second object if you want to override the first object
+
+            computers[i] = {
+                ...defaultProps,
+                ...computers[i]
+            };
+        };
+        console.log(computers);
+
+    });
+
     // your code here
 })();
