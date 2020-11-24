3c0c67ce92fef3460d1dbcd6cdbbaa59f6369423
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-24 09:55:33 +0100

    Answer exercise 4

--- 07-dom/04-attr-create/script.js
+++ 07-dom/04-attr-create/script.js
@@ -1,16 +1,15 @@
-/* becode/javascript
- *
- * /06-dom/04-attr-create/script.js - 6.4: manipulation d'attributs & création d'élément
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
 
-    // your code here
+
+ const dataImageValue = document.getElementById("source").getAttribute("data-image");
+
+ const createImage = document.createElement("IMG");
+ createImage.src = dataImageValue;
+ document.querySelector("#target").appendChild(createImage);
+
+
+ document.getElementById("source").remove();
+ // your code here
 
 })();
