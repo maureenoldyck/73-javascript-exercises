909c7bcd19e9e18d8bfab87ffb8ba0de1da0a274
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-24 10:46:13 +0100

    Answer hover image

--- 07-dom/05-hover-image/script.js
+++ 07-dom/05-hover-image/script.js
@@ -1,15 +1,23 @@
-/* becode/javascript
- *
- * /06-dom/05-hover-image/script.js - 6.5: survol d'image
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
+
+
+ const source =   document.querySelector("img").getAttribute("src");
+ const dataHover = document.querySelector("img").getAttribute("data-hover");
+
+document.querySelector("img").addEventListener("mouseover", imageHover);
+document.querySelector("img").addEventListener("mouseout", imageNormal);
+    
+
+function imageHover() {
+document.querySelector("img").src = dataHover;
+}
+
+function imageNormal() {
+document.querySelector("img").src = source;
+}
+
 
     // your code here
 
