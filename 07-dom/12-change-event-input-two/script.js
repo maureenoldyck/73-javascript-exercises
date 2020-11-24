4f02e20726cc873960b3e5cf8ec231e1f0c9c41d
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-24 19:12:37 +0100

    Answer change event input two

--- 07-dom/12-change-event-input-two/script.js
+++ 07-dom/12-change-event-input-two/script.js
@@ -1,15 +1,23 @@
-/* becode/javascript
- *
- * /06-dom/12-change-event-input-two/script.js - 6.12: événement change (2)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
+    document.querySelector("#pass-one").setAttribute("minlength", "8");
+    document.querySelector("#pass-one").addEventListener("keyup", type);
+
+
+    function type() {
+
+            const regex = /\d{2}/;
+            let numberCheck = regex.test(document.querySelector("#pass-one").value);
+
+        if (document.querySelector("#pass-one").value.length >= 8 && numberCheck == true) {
+            document.querySelector("#validity").innerHTML = "ok";
+        } else {
+            document.querySelector("#validity").innerHTML = "Not ok";
+
+        }
+    };
+
 
     // your code here
 
