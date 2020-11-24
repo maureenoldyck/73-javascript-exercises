549cb9d3c8a3f4b6f9b2b6c82ab198a2e55608fb
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-24 09:36:41 +0100

    Answer exercise select three

--- 07-dom/03-select-three/script.js
+++ 07-dom/03-select-three/script.js
@@ -1,15 +1,10 @@
-/* becode/javascript
- *
- * /06-dom/03-select-three/script.js - 6.3: sélection multiple par sélecteur css
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
+
+const text = document.querySelectorAll(".target");
+for (let i = 0; i < text.length; i++) {
+    text[i].innerHTML = "Owned.";
+}
 
     // your code here
 
