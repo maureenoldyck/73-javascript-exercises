ac0271b530363336b2552f6743e52f70ebdecceb
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-11-24 12:40:51 +0100

    Answer generate table one

--- 07-dom/07-generate-table-one/script.js
+++ 07-dom/07-generate-table-one/script.js
@@ -1,15 +1,21 @@
-/* becode/javascript
- *
- * /06-dom/07-generate-table-one/script.js - 6.7: génération d'un tableau (1)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
+(function () {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(function() {
+
+    const target = document.querySelector("#target");
+
+    const table = document.createElement("table");
+
+    for (let r = 0; r < 10; r++) {
+        const row = document.createElement("tr");
+        const cell = document.createElement("td");
+
+        table.appendChild(row);
+        row.appendChild(cell);
+    };
+
+
+    target.appendChild(table);
 
     // your code here
 
